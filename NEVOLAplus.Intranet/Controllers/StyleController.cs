66efbfc9805ef0ef7b17c15a87dfa495640f2c43
Michal <m.p.garnuch@gmail.com>
2025-06-20 21:23:30 +0200

    Manipulacja css z poziomu intranetu / uwaga narazei po rebuildingu

---
+++ NEVOLAplus.Intranet/Controllers/StyleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace NEVOLAplus.Intranet.Controllers
+{
+    public class StyleController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        public StyleController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        private string GetPortalCssPath()
+        {
+            return Path.Combine(_env.ContentRootPath, "..", "NEVOLAplus.Portal", "wwwroot", "css", "custom.css");
+        }
+
+        public IActionResult Edit()
+        {
+            var path = GetPortalCssPath();
+            var css = System.IO.File.Exists(path) ? System.IO.File.ReadAllText(path) : string.Empty;
+            ViewBag.CssContent = css;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string cssContent)
+        {
+            var path = GetPortalCssPath();
+            System.IO.File.WriteAllText(path, cssContent ?? string.Empty);
+            return RedirectToAction(nameof(Edit));
+        }
+    }
+}
