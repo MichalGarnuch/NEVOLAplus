6ce22431ab80974bd3be75cd013ccabd6eec2ec9
Michal <m.p.garnuch@gmail.com>
2025-06-21 12:00:22 +0200

    Dodano okno logowania z backgroudnem

---
+++ NEVOLAplus.Portal/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEVOLAplus.Portal.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Tu w przyszłości można dodać obsługę uwierzytelniania
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            // Po wylogowaniu wyświetlamy stronę logowania
+            return RedirectToAction("Login");
+        }
+    }
+}
